954e9d388c3647d3edc46ea413e49b4523e14c84
Yura <ypalii2012@gmail.com>
2015-05-19 15:37:28 +0300

    added MainBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Chdtu\UserBundle\ChdtuUserBundle(),
+            new Chdtu\MainBundle\ChdtuMainBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Chdtu/MainBundle/ChdtuMainBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Chdtu\MainBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ChdtuMainBundle extends Bundle
+{
+}
