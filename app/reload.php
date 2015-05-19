3df9996326583cabb9a05d849a490d192c545638
Yura <ypalii2012@gmail.com>
2015-05-19 11:51:23 +0300

    fixed reload

--- app/reload.php
+++ app/reload.php
@@ -7,8 +7,8 @@ function doCliAction($description, $command) {
     passthru($command);
 }
 doCliAction('Changing permissions', 'sudo chmod -R 777 app/cache app/logs web/');
-doCliAction('Create database schema', 'php app/console doctrine:mongodb:schema:create');
-doCliAction('Load fixtures to database', 'php app/console doctrine:mongodb:fixtures:load');
+doCliAction('Create database schema', 'php app/console doctrine:schema:create');
+//doCliAction('Load fixtures to database', 'php app/console doctrine:mongodb:fixtures:load');
 doCliAction('Clear cache', 'php app/console cache:clear');
 doCliAction('Install assets', 'php app/console assets:install --symlink');
 //doCliAction('Create admin user', 'php app/console fos:user:create admin admin@example.com samurai --super-admin');
